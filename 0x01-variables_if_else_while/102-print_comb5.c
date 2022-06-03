8b533e53e37352b6064f393c9acc8bab62548ad8
mkiki254 <kariukicharles440@gmail.com>
2022-06-03 12:05:14 +0000

    Printing all possible combinations of two digits from 00 to 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,24 +11,24 @@ int main(void)
 {
 	int i, j;
 
-	for(i = 0; i <= 99; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for(j = 0; j <= 99; j++)
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j && i != j)
 			{
-				if(i < j && i != j)
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 98 || j != 99)
 				{
-					putchar((i / 10) + '0');
-					putchar((i % 10) + '0');
+					putchar(',');
 					putchar(' ');
-					putchar((j /10) + '0');
-					putchar((j % 10) + '0');
-					if (i != 98 || j != 99)
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
 			}
+		}
 	}
 	putchar('\n');
 	return (0);
