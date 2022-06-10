fb13740b597a115def4ad1f839ed61cb6e6e31c4
mkiki254 <kariukicharles440@gmail.com>
2022-06-10 09:15:18 +0000

    Multiplying integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: returns result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 Task 0: Function that checks whether alphabet is uppercase
 Task 1: Function that checks whether element is a number
+Task 2: Function that multiplies two integers
 
