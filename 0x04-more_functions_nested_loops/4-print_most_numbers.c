8e378f067340387a02aac3cb22cf01a4fbf8b11a
mkiki254 <kariukicharles440@gmail.com>
2022-06-10 09:54:57 +0000

    Printing numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,13 @@
 void print_most_numbers(void)
 {
 	int i;
-	char str[8] = "01356789";
 
-	for (i = 0; i <= 8; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		_putchar(str[i]);
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
 	}
 	_putchar('\n');
 }
