7182d9d4ecbeb5148409bff1e78a203eff3c85eb
mkiki254 <kariukicharles440@gmail.com>
2022-06-22 11:06:24 +0000

    fixing power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,12 +12,9 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else
-	{
-		if (y == 0)
-			return (1);
-		if (y == 1)
-			return (x);
-		return (_pow_recursion(x, y - 1) * x);
-	}
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (_pow_recursion(x, y - 1) * x);
 }
