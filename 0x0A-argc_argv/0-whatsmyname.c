b2dc797a7c1bc31ece2fb5df046ff0ef6124a62a
mkiki254 <kariukicharles440@gmail.com>
2022-06-24 17:06:02 +0000

    fixing program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the program
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
