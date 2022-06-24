6edec6bda82a1232bc6057405248229466580461
mkiki254 <kariukicharles440@gmail.com>
2022-06-24 21:10:15 +0000

    coins exchange

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Shows the numbers of coins to be returned
+ * @argc: number of arguments
+ * @argv: argument array;
+ *
+ * Return: 0 if success, 1 if error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int mon, change = 0;
+
+		mon = atoi(argv[1]);
+		if (mon < 0)
+			printf("%d\n", change);
+		while (mon > 0)
+		{
+			if (mon >= 25)
+				mon -= 25;
+			else if (mon >= 10)
+				mon -= 10;
+			else if (mon >= 5)
+				mon -= 5;
+			else if (mon >= 2)
+				mon -= 2;
+			else
+				mon -= 1;
+			change += 1;
+		}
+		printf("%d\n", change);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -3,4 +3,4 @@ Task 1: The program prints the number of arguments passed into it
 Task 2: The program prints all the arguments its recieves.
 Task 3: The program multiplies two numbers passed as arguments.
 Task 4: The program adds positive numbers
-
+Task 5: The program shows the number of cents exchanged for an amount of money
